303a598ddc918606a0a7a73de754ca5843ecd075
Niko0004Stud <niko0004@stud.ek.dk>
2025-12-05 14:42:44 +0100

    Kald af loadFiles()

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        FileManager.loadFiles();
         Scanner keyboard=new Scanner(System.in);
         UserInput test=new UserInput(keyboard);
         test.menu();
--- src/MotionistSvømmer.java
+++ src/MotionistSvømmer.java
@@ -3,6 +3,11 @@ public class MotionistSvømmer extends Svømmer {
     public MotionistSvømmer(String navn, int alder, String tlf, String status){
         super(navn, alder, tlf, status, "motionist");
     }
+
+    public void testfM(){
+        FileManager fM = new FileManager();
+        //fM.gemTilFil();
+    }
 } //Motionist svømmer er meget det samme som Svømmer, men aktivitet er en fast værdi
 //"motionist", plus Svømmer er abstract og der kan ikke oprettes objekter derfra,
 //Svømmer skal bruges til at nedarve fra.
