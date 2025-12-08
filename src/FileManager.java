2b4f53debed6f1005555ca7749cb2bee387b0a08
Niko0004Stud <niko0004@stud.ek.dk>
2025-12-08 09:20:08 +0100

    Har gjort koden i klassen Fil lidt pænere

--- src/FileManager.java
+++ src/FileManager.java
@@ -8,11 +8,8 @@ public class FileManager {
     static ArrayList<MotionistSvømmer> motionistFil = new ArrayList<>();
     //ArrayList<TestObjekt> fil2;
 
-    /*public FileManager() throws FileNotFoundException {
-        listOfLists.add(list1);
-        listOfLists.add(list2);
-    } */
-
+    // Tekstlinjerne fra alle filer læses, bliver omdannet til deres respæktive objekttyper,
+    // som så bliver lagt i ArrayLister, der også passer med filerne - Altså er der en ArrayListe til hver fil.
     public static void loadFiles(){
         Boolean keepGoing = true;
         while (keepGoing){
@@ -22,9 +19,6 @@ public class FileManager {
                 while ((linje = bReader.readLine()) != null ){
                     String[] bidder = linje.split(";");
                     int antalBidder = bidder.length;
-
-
-                    // TestObjekt tON = TestObjekt.opretTestObjekt(bidder[0], bidder[1], bidder[2]);
                     motionistFil.add(new MotionistSvømmer(bidder[0], parseInt(bidder[1]), bidder[2], bidder[3]));
                 }
             } catch (IOException e){
@@ -33,10 +27,11 @@ public class FileManager {
             keepGoing = false;
         }
     }
-
+    // Gemmer IKKE på filen, men blot på en ArrayList, gemTilFil() skal kaldes for at gemme fra ArrayListen til dens fil
+    // Modtager navnet på filen der skal gemmes til, og tekststrengen som skal gemmes.
     public void tilføjTilArray(String filNavn, String filString){
         String[] bidder = filString.split(";");
-        switch (filNavn){
+        switch (filNavn){ // I første omgang gemmes tekststrengen som objekt i ArrayListen for den givne fil
             case "svømmerFil.txt":
                 motionistFil.add(new MotionistSvømmer(bidder[0], parseInt(bidder[1]), bidder[2], bidder[3]));
                 break;
@@ -44,45 +39,17 @@ public class FileManager {
         }
     }
 
-    /*public void gemAltTilFiler() throws IOException {
-        String filer = "svømmerFil.txt";
-        String[] filBidder = filer.split("-");
-        int antalFiler = filBidder.length;
-        for(int t=0; t<antalFiler; t++){
-            try(FileWriter filRydder = new FileWriter(filBidder[t], false)){
-                filRydder.write("");
-            } catch (IOException e){
-                System.out.println("Fejl ved rydning af fil: "+filBidder[t]);
-            }
+    // Modtager det teksten der skal gemmes, og hvilken fil
+    void skrivTilFil(String filNavn, String toWrite){
+        try (BufferedWriter bWriter = new BufferedWriter (new FileWriter(filNavn, true))){
 
-            try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(filBidder[t],true))){
-                for()
-            }
+            bWriter.newLine();
+        } catch (IOException e){
+            System.out.println("Fejl ved skrivning til Fil: "+filNavn);
         }
-
-    }*/
-
-    void test(){
-        //gemFiltest(FileTypes.SWIMMER);
     }
 
-    void skrivTilFil(String filename, String toWrite){
-
-    }
-
-    /*void gemAlt(String filename){
-            for(Entity2 e: list2){
-                skrivTilFil(filename, e.toTextFile());
-            }
-            //gem svømmer
-            Entity2 entity = new Entity2();
-            skrivTilFil("svømmere.txt", entity.toTextFile());
-
-            //for liste af tider
-            //gem Tid
-
-    }*/
-
+    // Skal ikke selv gemme til filen, for så se skrivTilFil() lige ovenfor
     public void gemTilFil(String filNavn){
         try (FileWriter filRydder = new FileWriter(filNavn,false)){
             filRydder.write("");
@@ -96,8 +63,9 @@ public class FileManager {
                 case "Motionister.txt":
                     antalObjekter = motionistFil.size();
                     for (int j=0; j<antalObjekter; j++){
-                        MotionistSvømmer tO = motionistFil.get(j);
-                        bWriter.write(tO.getAlt());
+                        //MotionistSvømmer tO = motionistFil.get(j);
+                        skrivTilFil(filNavn, motionistFil.get(j).getAlt());
+                        //bWriter.write(tO.getAlt());
                         bWriter.newLine();
                     }
                     break;
@@ -115,14 +83,8 @@ public class FileManager {
             System.out.println("Fejl ved indskrivning til Fil: "+filNavn);
         }
     }
-    /*public static void loadFile(){
-        svømmerFil = new ArrayList<>();
-        TestObjekt tON1 = TestObjekt.opretTestObjekt("1234");
-        TestObjekt tON2 = TestObjekt.opretTestObjekt("0000");
-        svømmerFil.add(tON1);
-        svømmerFil.add(tON2);
-    } */
 
+    // Bør være ligetil at lave nu
     public void redigerFil(String filNavn, int findIndex, int retIndex, String retTil){
 
     }
@@ -131,6 +93,7 @@ public class FileManager {
         MotionistSvømmer mS = motionistFil.remove(0);
     }
 
+    // Ikke sikker på hvad denne skal bruges til
     public String toString(int index){
         MotionistSvømmer mS = motionistFil.get(index);
         String tilString = mS.getTlf();
